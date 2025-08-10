bf4d2e2d4007ed56a3d8035920862d01b3f7c3e3
Diego Forero <dafe1311@gmail.com>
2025-08-09 20:08:16 -0500

    Correcciones Logica Tasks

--- mobile/lib/features/tasks/presentation/screens/tasks_screen.dart
+++ mobile/lib/features/tasks/presentation/screens/tasks_screen.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/features/shared/shared.dart';
-import 'package:mobile/features/tasks/domain/entities/task.dart';
-import 'package:mobile/features/tasks/presentation/providers/task_provider.dart';
-import 'package:provider/provider.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final taskProvider = context.watch<TaskProvider>();
-
     final scaffoldKey = GlobalKey<ScaffoldState>();
 
     return Scaffold(
@@ -24,7 +19,7 @@ class TasksScreen extends StatelessWidget {
           )
         ],
       ),
-      body: _TasksView(taskProvider.tasks),
+      body: _TasksView(),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Nueva Tarea'),
         icon: const Icon( Icons.add ),
@@ -36,23 +31,10 @@ class TasksScreen extends StatelessWidget {
 
 
 class _TasksView extends StatelessWidget {
-  final List<Task> tasks;
-
-  const _TasksView(this.tasks);
+  const _TasksView();
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: tasks.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text( tasks[index].title ),
-          subtitle: Text( tasks[index].description ),
-          trailing: tasks[index].isCompleted
-            ? const Icon(Icons.check_circle, color: Colors.green)
-            : const Icon(Icons.radio_button_unchecked, color: Colors.red),
-        );
-      },
-      );
+    return ListView();
   }
 }
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'
-    hide ChangeNotifierProvider;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile/config/config.dart';
-import 'package:mobile/features/tasks/infrastructure/repositories/task_repository_impl.dart';
-import 'package:mobile/features/tasks/presentation/providers/task_provider.dart';
-import 'package:provider/provider.dart';
+
 
 void main() async {
   await Environment.initEnvironment();
-  final taskRepository = TaskRepositoryImpl();
   runApp(
-    ProviderScope(
-      child: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => TaskProvider(taskRepository),
-          ),
-        ],
-        child: MainApp(),
-      ),
-    ),
+    const ProviderScope(child: MainApp())
   );
 }
 
@@ -27,8 +14,9 @@ class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final appRouter = ref.watch(goRouterProvider);
+  Widget build(BuildContext context, WidgetRef ref ) {
+
+    final appRouter = ref.watch( goRouterProvider );
 
     return MaterialApp.router(
       routerConfig: appRouter,
